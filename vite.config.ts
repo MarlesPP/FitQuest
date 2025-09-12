444464d1986efc22b650949e3199409accc78e84
MarlesPP <marioalfsuarez18@gmail.com>
2025-09-11 21:40:51 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // Esta línea toma la variable GEMINI_API_KEY del entorno de Vercel
-    // y la entrega al código cuando este pide process.env.API_KEY
-    'process.env.API_KEY': JSON.stringify(process.env.GEMINI_API_KEY)
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
